247389052ac71174de5622fd8c4b82cbf8d0cee7
Kushal Naidu <akushalnaidu@gmail.com>
2017-05-29 20:00:23 +0530

    Added more regions by popular demand

--- RL-PingFinder.py
+++ RL-PingFinder.py
@@ -4,11 +4,11 @@ Created on Sat May 20 22:03:13 2017
 
 @author: Kushal
 """
-#185.179.203.27 m-e
-#109.22.215.75 oceania
-#45.32.110.9 Ascxx
-#hkserverworks.com ascxxxxxx
-#72.5.161.230 ASC1000
+
+
+#Order of ips in text file : ascxx-ascxxxxxxxx-asc1000-jpn-MidEast-Oce-EUxxxxxxx-EUxx-EUxxx
+#eu has way too many servers, randomly picking 2 IP's and calculating. 
+
 import subprocess
 import re
 from tkinter import *
@@ -28,13 +28,12 @@ hosts=qq.split()
 #print(hosts)
 
 name=['?ms']*len(hosts)
-cmd="ping 72.5.161.230"
 
 line=''
 stringvar=[]
 for i in range(len(hosts)):
     stringvar.append(StringVar())
-    
+both = StringVar()
 continue_ping=False;
 
 def callback(event):
@@ -58,13 +57,14 @@ def StartWorker(worker):
         worker.start()
         
 
-
+both.set("{}/{}".format(stringvar[7].get(),stringvar[8].get()))
 def GetPings():
     global continue_ping
     continue_ping=True
     for i in range(len(stringvar)):
         stringvar[i].set(name[i])
     while(continue_ping):
+        both.set("{}/{}".format(stringvar[7].get(),stringvar[8].get()))
         for i in range(len(hosts)):
             try:
                 popen = subprocess.Popen(["ping",hosts[i],"-n","1"], stdout=subprocess.PIPE, bufsize=1,shell=True)
@@ -77,60 +77,66 @@ def GetPings():
     
             except:
                 name[i]='?ms'
-    #            try:
-    #                if(i==0):
-    #                    result = subprocess.check_output("ping 45.76.177.233 -n 1",shell=True)
-    #                    name[i]=(re.findall(r'Average = (.*?)ms',str(result))[0])
-    #                else:
-    #                    name[i]=0
-    #            except:
-                
+
             for i in range(len(stringvar)):
                 stringvar[i].set(name[i])
-#            stringvar[1].set("2")
+
             if not continue_ping:
                 break;
-#    print(continue_ping)
+                
     #worker._stop()
     main.destroy()
-                
+    
     
 
-main.title("Ping Finder")
-main.minsize(width=350, height=300)
-main.maxsize(width=350, height=300)
+main.title("Ping Tester")
+main.minsize(width=330, height=220)
+main.maxsize(width=330, height=220)
 
-label1 = Label( main, text="Ping to ASCxx :\n\nPing to ASCxxxxxx :\n\nPing to ASC1000/1001 :\n\nPing to Asia-East(JPN) :\n\nPing to Middle-East :\n\nPing to Oceania :" ,anchor=W, justify=RIGHT)
+label1 = Label( main, text="ASCxx :\n\nASCxxxxxx :\n\nASC1000/1001 :\n\nAsia-East(JPN) :" ,anchor=S, justify=RIGHT)
+label2 = Label( main, text="Middle-East :\n\nOceania :\n\nEUxxxxxx :\n\n EUxx :",anchor=S, justify=RIGHT)
+#label1 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+#label2 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+#label3 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+#label4 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+#label5 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+#label6 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+#label7 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+#label8 = Label( main, text="ASCxx" ,anchor=S, justify=RIGHT)
+
 ping1 = Label( main, textvariable=stringvar[0] , justify=RIGHT)
-
 ping2 = Label( main, textvariable=stringvar[1] , justify=RIGHT)
-
 ping3 = Label( main, textvariable=stringvar[2] , justify=RIGHT)
-
 ping4 = Label( main, textvariable=stringvar[3] , justify=RIGHT)
-
 ping5 = Label( main, textvariable=stringvar[4] , justify=RIGHT)
-
 ping6 = Label( main, textvariable=stringvar[5] , justify=RIGHT)
+ping7 = Label( main, textvariable=stringvar[6] , justify=RIGHT)
+ping8 = Label( main, textvariable=both , justify=RIGHT)
 
-label1.grid(row=4,column=0,rowspan=6,padx=0)
-ping1.grid(row=4,column=2)
-ping2.grid(row=5,column=2)
-ping3.grid(row=6,column=2)
-ping4.grid(row=7,column=2)
-ping5.grid(row=8,column=2)
-ping6.grid(row=9,column=2)
+
+
+label1.grid(row=4,column=0,rowspan=4,padx=0)
+label2.grid(row=4,column=2,rowspan=4,padx=0)
+ping1.grid(row=4,column=1,pady=4)
+ping2.grid(row=5,column=1,pady=4)
+ping3.grid(row=6,column=1,pady=4)
+ping4.grid(row=7,column=1,pady=4)
+ping5.grid(row=4,column=3,pady=4)
+ping6.grid(row=5,column=3,pady=3)
+ping7.grid(row=6,column=3,pady=3)
+ping8.grid(row=7,column=3,pady=3)
+
 worker = CreateWorker(GetPings)
 
-Bmain=Button(main, text ="Find Pings",command=lambda: StartWorker(worker))
+Bmain=Button(main, text ="Display Pings",command=lambda: StartWorker(worker))
 Bmain2=Button(main, text ="Exit",command=StopWorker)
 linkt = Label(main, text="For Suggestions/Feedback : ")
-link = Label(main, text="Steam ID", fg="blue", cursor="hand2")
-linkt.grid(row=11,column=0,columnspan=3,padx=5)
-link.grid(row=11,column=4,padx=5)
+link = Label(main, text="Steam ID                           ", fg="blue", cursor="hand2")
+linkt.grid(row=11,column=0,columnspan=2,padx=5)
+link.grid(row=11,column=2,columnspan=2,padx=5)
 link.bind("<Button-1>", callback)
-Bmain.grid(row=0,column=1,pady=10)
-Bmain2.grid(row=10,column=1,pady=10)
+Bmain.grid(row=0,column=0,columnspan=4,ipadx=10,pady=5)
+Bmain2.grid(row=10,column=0,columnspan=4,ipadx=10,pady=5)
 main.protocol("WM_DELETE_WINDOW", StopWorker)
 main.mainloop()
 
